a02e786b01a6ff8f2682f5daced914dbcb1ddf4c
Noor Karim <noorussabak@gmail.com>
2021-08-05 12:33:49 +1000

    1st version
    basic segue and navigation controller implementation

--- LoginApp/ViewController.swift
+++ LoginApp/ViewController.swift
@@ -9,11 +9,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var userNameTextField: UITextField!
+  
+    
+    @IBOutlet var forgotUsernameButton: UIButton!
+    
+    @IBOutlet var forgotPasswordButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+//
+//        switch segue.identifier {
+//        case "forgotUsernameOrPassword":
+//            segue.destination.navigationItem.title = "Forgot Username/Password"
+//        case "showLanding":
+//            segue.destination.navigationItem.title = userNameTextField.text
+//        default:
+//            segue.destination.navigationItem.title = "Landing"
+//
+//        }
+//
+        
+        guard let sender = sender as? UIButton else{return}
+        
+        switch sender {
+        case forgotUsernameButton:
+            segue.destination.navigationItem.title = "Forgot Username"
+        case forgotPasswordButton:
+            segue.destination.navigationItem.title = "Forgot Password"
+        default:
+            segue.destination.navigationItem.title = userNameTextField.text
+        }
+        
+    }
+    
+    @IBAction func forgotPasswordButtonAction(_ sender: Any) {
+        performSegue(withIdentifier: "forgotUsernameOrPassword", sender: sender)
+    }
+    
+    @IBAction func forgotUsernameButtonAction(_ sender: Any) {
+        performSegue(withIdentifier: "forgotUsernameOrPassword", sender: sender)
+    }
+    
 }
 
---
+++ LoginApp/landingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  landingViewController.swift
+//  LoginApp
+//
+//  Created by Noor-us-saba Karim on 5/8/21.
+//
+
+import UIKit
+
+class landingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
